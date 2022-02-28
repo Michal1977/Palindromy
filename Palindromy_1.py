473f6957dde28b5595c691e4ae8cfebc366526e5
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-02-28 21:45:59 +0100

    Limit number of interactions

--- Palindromy_1.py
+++ Palindromy_1.py
@@ -1,4 +1,6 @@
 def palindrom_check(word):
-    """funkcja sprawdza czy dany wyraz jest palindromem,
-    porównuje znaki stojące na tym samym miejscu wyrazu, licząc od 
-    początku i od końca"""
+  """funkcja sprawdza czy dany wyraz jest palindromem,
+  porównuje znaki stojące na tym samym miejscu wyrazu, licząc od początku i od końca"""
+  i = len(word)/2
+  n = 0
+  while n < i:

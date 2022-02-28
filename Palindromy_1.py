3807b6e5a2b39bbe6479be0c82aba616c34c15cf
Michał Jędrzejak <mjedrzejak77@gmail.com>
2022-02-28 21:57:00 +0100

    fix spacing and check if ok

--- Palindromy_1.py
+++ Palindromy_1.py
@@ -4,7 +4,11 @@ def palindrom_check(word):
   i = len(word)/2
   n = 0
   while n < i:
-    if word[n] != word[-n - 1]:
-      return False
+      if word[n] != word[-n - 1]:
+        return False
       n = n + 1
   return True
+
+print(palindrom_check('potop'))
+print(palindrom_check('fajka'))
+print(palindrom_check('ballab'))

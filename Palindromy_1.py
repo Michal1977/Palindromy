8651867f44a7443457dc4061e34da5936dfaf6da
Michal1977 <100101129+Michal1977@users.noreply.github.com>
2022-02-28 21:30:08 +0100

    Update Palindromy_1.py
    Update function name and description

--- Palindromy_1.py
+++ Palindromy_1.py
@@ -1,2 +1,4 @@
-def palindrom_check():
-    """funkcja sprawdza czy dany wyraz jest palindromem"""
+def palindrom_check(word):
+    """funkcja sprawdza czy dany wyraz jest palindromem,
+    porównuje znaki stojące na tym samym miejscu wyrazu, licząc od 
+    początku i od końca"""
